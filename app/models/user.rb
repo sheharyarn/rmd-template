87f3400b16f3c79bf8cf727b12a1b37493a73057
Sheharyar Naseer <sheharyar92@live.com>
2014-03-19 01:00:01 +0500

    Removed extra items from user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,57 +3,7 @@ class User
   include Mongoid::Timestamps
   # include Mongoid::Paranoia
 
-  embeds_one :access
-  has_many   :logs,           :class_name => "Log",              :inverse_of => :logger
-  has_many   :item_creations, :class_name => "Item",             :inverse_of => :creator
-  has_many   :customer_sales, :class_name => "CustomerPurchase", :inverse_of => :cashier
-  has_and_belongs_to_many :branches
-  
-  field :name,			:type => String, 	:default => ""
-  field :status,		:type => String, 	:default => ""
-
-  validates :access, :presence => true
-
-  # before_save  :access_should_exist
-  after_create :normalize_abilities
-
-  # def access_should_exist
-  #   if self.access.nil?
-  #     self.access = Access.new
-  #   end
-  # end
-
-  def normalize_abilities
-    self.access.save
-  end
-
-
-  # Role Methods
-  def can? action, branch, type
-    return self.access.can? action, branch, type
-  end
-  
-  def cannot? action, branch, type
-    return !self.can?(action, branch, type)
-  end
-
-  def role_is? role
-    return self.access.role_is? role
-  end
-
-  def role_above? role
-    return self.access.role_above? role
-  end
-
-  def role_is_and_above? role
-    return (self.role_is?(role) || self.role_above?(role))
-  end
-
-  def role_below? role
-    return !self.role_is_and_above?(role)
-  end
-
-
+  # ENTER USER-SPECIFIC STUFF HERE
 
   ###### Devise Modules ##################################################
   devise :database_authenticatable, :registerable,
