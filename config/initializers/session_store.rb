9109e2be4820d3319224a6c461ac5b3af08dedb0
Sheharyar Naseer <sheharyar92@live.com>
2014-03-19 00:51:29 +0500

    Fix a few attributes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-RmdTemplate::Application.config.session_store :cookie_store, key: '_edu-inv_session'
+RmdTemplate::Application.config.session_store :cookie_store, key: 'rmdtemplate_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
